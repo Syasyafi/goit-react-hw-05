737bb782b5d5fa63a259a032a723cb3a2598b334
Nastya <nastya.syasyafi@gmail.com>
2024-10-02 23:49:25 +0300

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 import css from "./MovieList.module.css";
 
-export default function MovieList({ movie, state }) {
+export default function MovieList({ movie }) {
+  const location = useLocation();
   return (
     <>
       <ul>
         {movie.map((item) => (
           <li key={item.id}>
-            <Link to={`/movies/${item.id}`} state={state} className={css.text}>
+            <Link
+              to={`/movies/${item.id}`}
+              state={location}
+              className={css.text}
+            >
               {item.title}
             </Link>
           </li>
